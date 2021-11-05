e409aa3feeb40dc54adfb2309b2f9e4e0c1ea07f
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-11-05 15:38:38 +0100

    did some cleaning

--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private GameObject target;
-    private CharacterMovementController movementController;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectsWithTag("Tower")[0];
-
-        movementController = gameObject.GetComponent<CharacterMovementController>();
     }
     
     void Update()
     {
         // calculate the vector3 direction towards the target
         Vector3 dif = (target.transform.position - transform.position);
-        
-        movementController.Move(dif, 1f);
+
+        transform.position += dif.normalized * Time.deltaTime;
     }
 }
