09b115c3f088fe2ab39bd437a4dbea76130ab642
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-12-19 17:20:20 +0100

    better enemy targeting

--- Assets/Scripts/Characters/Enemies/GenericEnemyController.cs
+++ Assets/Scripts/Characters/Enemies/GenericEnemyController.cs
@@ -1,72 +1,94 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using Object = UnityEngine.Object;
 
 [Serializable]
-public struct EnemyTowerBehaviour
+public struct EnemyBehaviour
 {
-    public string Name;
-    public float MinDistance;
+    public float MinAttackDistance;
     public bool AttackWhenInRang;
     public bool AttackWhenDamaged;
+    public float MinMoveDistance;
     public bool MoveTowardsWhenInRange;
     public bool MoveTowardsWhenDamaged;
 }
 
 public class GenericEnemyController : MonoBehaviour
 {
-    public List<EnemyTowerBehaviour> behaviours = new List<EnemyTowerBehaviour>();
+    public EnemyBehaviour behaviour;
 
-    private GameObject _activeTarget;
+    private GameObject _activeMovementTarget;
+    private GameObject _activeAttackTarget;
     private NavMeshAgent _agent;
+
+    private static GameObject _stronghold;
     
-    // Start is called before the first frame update
     void Start()
     {
+        _stronghold = GameObject.FindGameObjectsWithTag("Stronghold")[0];
         _agent = GetComponent<NavMeshAgent>();
         InvokeRepeating("UpdateTarget", 0, 1);
     }
     
+    /// <summary>
+    /// Finds target to attack / move towards
+    /// </summary>
     void UpdateTarget()
     {
-        GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
+        List<GameObject> targets = GameObject.FindGameObjectsWithTag("Tower").ToList();
+
+        targets.Add(_stronghold); // add stronghold so it gets attacked when closest
         
         bool success = false;
         GameObject closest = null;
-        EnemyTowerBehaviour closestBehaviour = new EnemyTowerBehaviour();
         float closestDistance = Single.MaxValue;
         
         // loop through all towers
-        foreach (GameObject tower in towers)
+        foreach (GameObject t in targets)
         {
-            EnemyTowerBehaviour behaviour = behaviours.Find(e => e.Name == tower.GetComponent<TowerDescriptor>().name);
-            
-            float distance = Vector3.Distance(transform.position, tower.transform.position);
+            float distance = Vector3.Distance(transform.position, t.transform.position);
 
             if (distance > closestDistance) continue; // skip if not closest
-            if (distance > behaviour.MinDistance) continue; // skip if further then minumum distance
 
+            // save closest
             success = true;
-            closest = tower;
-            closestBehaviour = behaviour;
+            closest = t;
             closestDistance = distance;
         }
         
-        if (success && closestBehaviour.MoveTowardsWhenInRange)
+        // set pathfinding target
+        if (success && closestDistance < behaviour.MinMoveDistance && behaviour.MoveTowardsWhenInRange)
         {
-            _activeTarget = closest;
+            _activeMovementTarget = closest;
         }
         else
         {
-            _activeTarget = GameObject.FindGameObjectsWithTag("Stronghold")[0];
+            // move towars stronghold by default
+            _activeMovementTarget = _stronghold;
+        }
+        
+        // set target to attack
+        if (success && closestDistance < behaviour.MinAttackDistance  && behaviour.AttackWhenInRang)
+        {
+            _activeAttackTarget = closest;
         }
     }
 
     private void FixedUpdate()
     {
-        _agent.destination = _activeTarget.transform.position;
+        _agent.destination = _activeMovementTarget.transform.position;
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(_activeMovementTarget.transform.position, 0.5f);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_activeAttackTarget.transform.position, 0.5f);
     }
 }
