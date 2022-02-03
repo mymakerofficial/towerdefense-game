460ede49ddf420203f6d86cfdf24c4480a51bb73
Louis <81430377+lousystems@users.noreply.github.com>
2022-02-03 17:48:49 +0100

    moved InsufficientCreditException into own cs file

--- Assets/Scripts/Player/CreditController.cs
+++ Assets/Scripts/Player/CreditController.cs
@@ -82,33 +82,3 @@ public class CreditController : MonoBehaviour
         return _credit - amount > 0;
     }
 }
-
-public class InsufficientCreditException : Exception
-{
-    /// <summary>
-    /// Credits Needed for operation 
-    /// </summary>
-    public long CreditNeeded { get; }
-    /// <summary>
-    /// given Credits 
-    /// </summary>
-    public long CreditProvided { get; }
-    
-
-    public InsufficientCreditException(string message,Exception innerException, long creditNeeded, long creditProvided) : base(message,innerException)
-    {
-        this.CreditNeeded = creditNeeded;
-        this.CreditProvided = creditProvided;
-    }
-    public InsufficientCreditException(string message, long creditNeeded, long creditProvided) : base(message)
-    {
-        this.CreditNeeded = creditNeeded;
-        this.CreditProvided = creditProvided;
-    }
-    
-    public InsufficientCreditException(long creditNeeded, long creditProvided)
-    {
-        this.CreditNeeded = creditNeeded;
-        this.CreditProvided = creditProvided;
-    }
-}
